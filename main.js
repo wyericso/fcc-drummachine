75a2bfb0da1d942b66aae726714c60e3f7cd1e5b
wyericso <eric@woooa.net>
2018-10-03 22:35:21 +0800

    Initial build.

--- main.js
+++ main.js
@@ -6,16 +6,45 @@ class DrumMachine extends React.Component {
     }
 
     render() {
-        return (<span>#display</span>);
+        return (
+            <div id="drum-machine">
+                <div id="display"></div>
+                <Drumpads />
+            </div>
+        );
     }
 }
 
-ReactDOM.render(<DrumMachine />, document.getElementById('display'));
+class Drumpads extends React.Component {
+    constructor(props) {
+        super(props);
+        this.getKey = this.getKey.bind(this);
+    }
 
-document.addEventListener('keydown', (event) => {
-    console.log(event.key.toUpperCase());
-    document.getElementById(event.key.toUpperCase()).style.backgroundColor = 'blue';
-    window.setTimeout(() => {
-        document.getElementById(event.key.toUpperCase()).style.backgroundColor = 'white';
-    }, 100);
-});
+    getKey(event) {
+        event.persist();
+        console.log(event.key.toUpperCase());
+        document.getElementById(event.key.toUpperCase()).style.backgroundColor = 'blue';
+        window.setTimeout(() => {
+            document.getElementById(event.key.toUpperCase()).style.backgroundColor = 'white';
+        }, 100);
+    }
+
+    render() {
+        return (
+            <div id="drumpads-container" onKeyDown={this.getKey} tabIndex="0">
+                <div className="drumpad" id="Q">Q</div>
+                <div className="drumpad" id="W">W</div>
+                <div className="drumpad" id="E">E</div>
+                <div className="drumpad" id="A">A</div>
+                <div className="drumpad" id="S">S</div>
+                <div className="drumpad" id="D">D</div>
+                <div className="drumpad" id="Z">Z</div>
+                <div className="drumpad" id="X">X</div>
+                <div className="drumpad" id="C">C</div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<DrumMachine />, document.body);

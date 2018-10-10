07e86768b319892c4efde131a826c26d1b1657d1
wyericso <eric@woooa.net>
2018-10-10 15:04:54 +0800

    Initial build.

--- main.js
+++ main.js
@@ -33,20 +33,47 @@ const mapStateToProps = state => {
     };
 };
 
-const keyToDisplay = {
-    Q: "Crash",
-    W: "Hi Tom",
-    E: "Mid Tom",
-    A: "Open HH",
-    S: "Snare",
-    D: "Lo Tom",
-    Z: "Closed HH",
-    X: "Kick",
-    C: "Ride"
+const keyMapping = {
+    Q: {
+        display: "Crash",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137499/fcc/drum-machine/209871__veiler__right-crash.mp3"
+    },
+    W: {
+        display: "Hi Tom",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137490/fcc/drum-machine/261407__veiler__tom-1.mp3"
+    },
+    E: {
+        display: "Mid Tom",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137490/fcc/drum-machine/261412__veiler__tom-2.mp3"
+    },
+    A: {
+        display: "Open HH",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137614/fcc/drum-machine/Dry_Ohh.mp3"
+    },
+    S: {
+        display: "Snare",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137731/fcc/drum-machine/Brk_Snr.mp3"
+    },
+    D: {
+        display: "Lo Tom",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137490/fcc/drum-machine/261411__veiler__tom-3.mp3"
+    },
+    Z: {
+        display: "Closed HH",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137699/fcc/drum-machine/Bld_H1.mp3"
+    },
+    X: {
+        display: "Kick",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137773/fcc/drum-machine/RP4_KICK_1.mp3"
+    },
+    C: {
+        display: "Ride",
+        audioUrl: "https://res.cloudinary.com/woooanet/video/upload/v1539137498/fcc/drum-machine/84234__josomebody__ride.mp3"
+    }
 };
 
 const ConnectedDisplay = ({ keyPressed }) => (
-    <div id="display">{ keyToDisplay[keyPressed] }</div>
+    <div id="display">{ keyMapping[keyPressed] && keyMapping[keyPressed].display }</div>
 );
 
 const Display = connect(mapStateToProps)(ConnectedDisplay);
@@ -60,87 +87,43 @@ const mapDispatchToProps = dispatch => {
 class ConnectedDrumpads extends React.Component {
     constructor(props) {
         super(props);
+        this.hitPad = this.hitPad.bind(this);
         this.handleKeyDown = this.handleKeyDown.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    hitPad(keyPressed) {
+        document.getElementById(keyPressed).style.backgroundColor = 'blue';
+        window.setTimeout(() => {
+            document.getElementById(keyPressed).style.backgroundColor = 'white';
+        }, 100);
+        const sound = document.getElementById("audio-" + keyPressed);
+        sound.currentTime = 0;
+        sound.play();
+        this.props.changeKey(keyPressed);
     }
 
     handleKeyDown(event) {
         event.persist();
-        document.getElementById(event.key.toUpperCase()).style.backgroundColor = 'blue';
-        window.setTimeout(() => {
-            document.getElementById(event.key.toUpperCase()).style.backgroundColor = 'white';
-        }, 100);
-        const sound = document.getElementById("audio-" + event.key.toUpperCase());
-        sound.currentTime = 0;
-        sound.play();
-        this.props.changeKey(event.key.toUpperCase());
+        this.hitPad(event.key.toUpperCase());
+    }
+
+    handleClick(keyPressed) {
+        this.hitPad(keyPressed);
     }
 
     render() {
         return (
             <div id="drumpads-container" onKeyDown={this.handleKeyDown} tabIndex="0">
-                <div className="drumpad" id="Q">
-                    Q
-                    <audio id="audio-Q">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137499/fcc/drum-machine/209871__veiler__right-crash.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className="drumpad" id="W">
-                    W
-                    <audio id="audio-W">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137490/fcc/drum-machine/261407__veiler__tom-1.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className="drumpad" id="E">
-                    E
-                    <audio id="audio-E">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137490/fcc/drum-machine/261412__veiler__tom-2.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className="drumpad" id="A">
-                    A
-                    <audio id="audio-A">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137614/fcc/drum-machine/Dry_Ohh.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className="drumpad" id="S">
-                    S
-                    <audio id="audio-S">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137731/fcc/drum-machine/Brk_Snr.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className="drumpad" id="D">
-                    D
-                    <audio id="audio-D">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137490/fcc/drum-machine/261411__veiler__tom-3.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className="drumpad" id="Z">
-                    Z
-                    <audio id="audio-Z">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137699/fcc/drum-machine/Bld_H1.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className="drumpad" id="X">
-                    X
-                    <audio id="audio-X">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137773/fcc/drum-machine/RP4_KICK_1.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
-                <div className="drumpad" id="C">
-                    C
-                    <audio id="audio-C">
-                        <source src="https://res.cloudinary.com/woooanet/video/upload/v1539137498/fcc/drum-machine/84234__josomebody__ride.mp3" type="audio/mpeg" />
-                        Your browser does not support the audio element.
-                    </audio>
-                </div>
+                {Object.keys(keyMapping).map((key) => 
+                    <div className="drumpad" id={key} key={key} onClick={() => this.handleClick(key)}>
+                        {key}
+                        <audio id={"audio-" + key}>
+                            <source src={keyMapping[key].audioUrl} type="audio/mpeg" />
+                            Your browser does not support the audio element.
+                        </audio>
+                    </div>
+                )}
             </div>
         );
     }
